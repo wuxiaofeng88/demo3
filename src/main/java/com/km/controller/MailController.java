fb8f80a5ed0d53dea69ca94d8bf8c915c62628cb
wuxiaofeng <1198229050@qq.com>
2019-06-26 12:10:07 +0800

    发送带图片的多媒体邮件

--- src/main/java/com/km/controller/MailController.java
+++ src/main/java/com/km/controller/MailController.java
@@ -6,8 +6,12 @@ package com.km.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 /**
  * @ClassName MailController
@@ -29,6 +33,23 @@ public class MailController {
         simpleMailMessage.setTo("920302147@qq.com");
         simpleMailMessage.setFrom("1198229050@qq.com");
         mailSender.send(simpleMailMessage);
+        System.out.println("邮件发送完成！");
+        return "success";
+    }
+
+    @GetMapping(value = "/sendMultiPartMail")
+    public String sendMultiPartMail() throws Exception{
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+        mimeMessageHelper.setSubject("亲爱的！");
+        mimeMessageHelper.setText("<b stype='color:red'>亲爱的，送你两张图片，请注意查收!</b>",true);
+        mimeMessageHelper.setTo("920302147@qq.com");
+        mimeMessageHelper.setFrom("1198229050@qq.com");
+
+        //添加图片
+        mimeMessageHelper.addAttachment("1.jpg",new File("D:\\测试用文件\\1.jpg"));
+        mimeMessageHelper.addAttachment("2.jpg",new File("D:\\测试用文件\\2.jpg"));
+        mailSender.send(mimeMessage);
         System.out.println("邮件发送完成！");
         return "success";
     }
